858dac9837de5e27d5da4b12d56ba9f096c5ed97
Gabriel Damasceno <gabriel.d.cruz@ba.estudante.senai.br>
2026-02-06 16:17:52 -0300

    Create atividade1.java

---
+++ atividade1.java
@@ -0,0 +1,13 @@
+public class atividade1 {
+
+    public static void main(string[] args){
+        int valor = 10;
+        System.out.println("vlaor é: " + valor);
+
+        int soma 5 + 5;
+        System.out.println("valor da soma é: " + soma);
+
+        int idade = 25;
+        System.out.println("a idade é:" + idade * 2);
+    }
+}
