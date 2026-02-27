5918eb8a3c180bb06b5aad263c54b0ccda52e080
Gabriel Damasceno <gabriel.d.cruz@ba.estudante.senai.br>
2026-02-27 16:16:02 -0300

    Create Aluno.java

---
+++ projeto_Aluno/Aluno.java
@@ -0,0 +1,87 @@
+public class Aluno {
+
+    private String name;
+    private int age;
+    private String registration;
+
+    // Construtor vazio
+    public Aluno() {
+    }
+
+    // Construtor com parâmetros
+    public Aluno(String name, int age, String registration) {
+        this.name = name;
+        this.age = age;
+        this.registration = registration;
+    }
+
+    // Getter e Setter para name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter e Setter para age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter e Setter para registration
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+}public class Aluno {
+
+    private String name;
+    private int age;
+    private String registration;
+
+    // Construtor vazio
+    public Aluno() {
+    }
+
+    // Construtor com parâmetros
+    public Aluno(String name, int age, String registration) {
+        this.name = name;
+        this.age = age;
+        this.registration = registration;
+    }
+
+    // Getter e Setter para name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter e Setter para age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter e Setter para registration
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+}
